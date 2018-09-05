d0212258631fbb171de249de2cd8b781970b919d
kitada4010 <bnzz4010@yahoo.co.jp>
2018-09-05 16:52:19 +0900

    unix修正

--- unix/bzdir.sh
+++ unix/bzdir.sh
@@ -3,14 +3,15 @@
 Usage(){
     echo "bzdir <ディレクトリ名>"
     echo "でディレクトリを圧縮したtar.bz2が作成できます。"
+    exit 0
 }
 
 #----------------------------------------------#
 
 if [ ! -d  $1 ]; then
     echo "ディレクトリが存在しません。"
+    Usage
 elif [ $# -ne '1' ]; then
     Usage
-else
-    tar -zcvf $1.tar.bz2 $1
 fi
+tar -zcvf $1.tar.bz2 $1
--- unix/gz2bz2.sh
+++ unix/gz2bz2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-ls | while read line
+ls | grep .gz | while read line
 do
-    if [ ${line##*.} = 'gz' ]; then
-	wc -c $line
+   	wc -c $line
 	gzip -d $line 
 	bzip2  ${line%.*}
 	wc -c ${line%.*}.bz2
-    fi
 done 
