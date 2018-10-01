8a7bba8b9ea6e8b6423c2ae6afd01575893579c0
kitada4010 <bnzz4010@yahoo.co.jp>
2018-10-01 23:04:24 +0900

    修正済み

--- C/work7-1_b.c
+++ C/work7-1_b.c
@@ -2,12 +2,12 @@
 #include "mylib.h"
 
 #define X0 1.0
-#define DIVIDE_NUM 10000000
+#define DIVIDE_NUM 100
 #define END 1.0
 #define MAX 256
 
 int main(void){
-  double x=X0, h=DIVIDE_NUM;
+  double x=X0, h=1.0/DIVIDE_NUM;
   int re=DIVIDE_NUM;
   int i;
   FILE *outfp;
@@ -15,7 +15,9 @@ int main(void){
 
   sprintf(fname, "%.7f-data.txt", h);
   outfp=fWopen(fname);
+  
   fprintf(outfp, "%lf\t%lf\n", 0.0, x);
+  
   for(i=1; i<=re; i++){
     x+=h*x;
     fprintf(outfp, "%lf\t%lf\n", (double)i*h, x);
--- C/work7-2_b.c
+++ C/work7-2_b.c
@@ -6,7 +6,7 @@
 #define T0 0.0 //初期時間
 #define X0 0.0 //初期位置(横軸)
 #define Y0 1.5 //初期位置(縦軸)
-#define H 0.0001 //刻み値
+#define DIVIDE_NUM 1000 
 #define G 9.8 //重力加速度
 #define FNAMEMAX 256 //ファイル名の最大文字数
 
@@ -20,9 +20,9 @@ void Usage(void){
 
 
 int main(int argc, char *argv[]){
-  double t=T0, x=X0, y=Y0, h=H, g=G; //t:初期時間 x:位置(横軸) y:位置(縦軸) h:刻み値 g:重力加速度
+  double t=T0, x=X0, y=Y0, h=1.0/DIVIDE_NUM, g=G; //t:初期時間 x:位置(横軸) y:位置(縦軸) h:刻み値 g:重力加速度
   double m,theta,v; //m:質量 theta:進行方向の角度 v:速度
-  double vsin, vcos; //vsin:縦方向の速度 vcos:横方向の速度
+  double v_y, v_x; //v_y:縦方向の速度 v_x:横方向の速度
   double time=0; //経過時間
   char fname[FNAMEMAX]={'\0'}; //ファイル名
   FILE* outfp;
@@ -35,21 +35,21 @@ int main(int argc, char *argv[]){
   
   theta=atof(argv[2]);
   
-  sprintf(fname,"m:%.3f_θ:%.2f_v:%.3f-data",m,theta,v); 
+  sprintf(fname, "m:%.3f_θ:%.2f_v:%.3f-data", m, theta, v); 
   outfp=fWopen(fname);
   
   theta=(theta*M_PI)/180.0;
   
-  vsin=v*sin(theta);
-  vcos=v*cos(theta);
+  v_y=v*sin(theta);
+  v_x=v*cos(theta);
   
-  fprintf(outfp,"%lf\t%lf\t%lf\n",t,x,y);
+  fprintf(outfp, "%lf\t%lf\t%lf\n", t, x, y);
 
   while(y>0){
-    x+=h*vcos;
+    x+=h*v_x;
     time+=h;
-    y+=h*((-g*time)+vsin);
-    fprintf(outfp,"%lf\t%lf\t%lf\n",t+time,x,y);
+    y+=h*((-g*time)+v_y);
+    fprintf(outfp, "%lf\t%lf\t%lf\n", t+time, x, y);
   }
   
   fclose(outfp);
